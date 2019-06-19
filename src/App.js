e900921e20c8554f27675d08dbd73654df2c7671
ccosat1isd <ccosat1isd@ccosat08isd.local>
2019-06-19 01:00:59 -0600

    Was able to access random fortune from daytime fortunes array

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ console.log(nightFortunes);
 // METHODS
 ////////////////
 
-// run one time only lifecycle method...
-// componentDidMount() {
-//    this.fetchList()
-// } <-- I think I only need this if I'm doing an API call.
+
 
 ////////////////
 // COMPONENT
@@ -26,10 +23,39 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      fortunes: '',
-      nightFortunes: ''
+      fortune:'',
+      userChoice: 'day'
     }
+    this.chooseRandomFortune = this.chooseRandomFortune.bind(this)
   }
+
+  // because we used this.state.userChoice in the chooseRandomFortune, we need to bind the "this" to the constructor.
+
+  componentDidMount() {
+    this.chooseRandomFortune()
+  } // not sure why we did this. Does it just access the data files?
+
+  chooseRandomFortune() {
+   if(this.state.userChoice === 'moonlight') {
+     // grab a random fortune from night
+     // randomVal is calculating a random index number x the length of one array
+     let randomVal = Math.floor(Math.random() * nightFortunes.fortunes.length)
+     let randomFortune = nightFortunes.fortunes[randomVal]
+     // update state
+     this.setState({
+       fortune: randomFortune
+     })
+     console.log('this is randomVal', randomVal, randomFortune)
+   } else {
+     // grab a random fortune from night
+     let randomDayVal = Math.floor(Math.random() * fortunes.fortunes.length)
+     let randomDayFortune = fortunes.fortunes[randomDayVal]
+     this.setState({
+       fortune: randomDayFortune
+     })
+   }
+  }
+
   render() {
     return (
       <div className="fortune-container">
@@ -37,12 +63,15 @@ class App extends Component {
           <h1>Good Fortune to You, Stranger...</h1>
           <h3>May the future smile upon you.</h3>
         </header>
+         <h4>{ this.state.fortune.fortune }</h4>
       </div>
     )
   }
 }
 
-// <h4>{ fortunes.sample }</h4> // <-- This did not work. Tried putting it in the render under the <h3>.
+// this.state.fortune.fortune refers to the state in the super constructor of "fortune" + the "fortune" key in the fortunes array.
+
+// <h4>{ fortunes.sample }</h4> // <-- This did not work. Tried putting it in the render under the <h3>. This didn't work because it's a Ruby method and doesn't work in JS.
 
 ////////////////
 // EXPORT
